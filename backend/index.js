81df546d3d19b9bfcd66ddca005f9fa11e12ed44
RW Sandaru Piumantha <132807012+RWSandaru8@users.noreply.github.com>
2025-06-21 20:52:34 +0530

    reports route

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,7 @@ import soapNotesRouter from './routes/soap-notes-routes.js';
 
 import studyRouter from './routes/study-routes.js';
 import radiologistRouter from './routes/radiologist-routes.js';
+import reportsRouter from './routes/reports-routes.js';
 
 dotenv.config();
 
@@ -70,5 +71,6 @@ app.use('/soap-notes', soapNotesRouter);
 
 app.use('/studies', studyRouter);
 app.use('/radiologists', radiologistRouter);
+app.use('/reports', reportsRouter);
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
